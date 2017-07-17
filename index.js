812e6596a1d50ad61af95e4a7fe40f026c1dd3fe
aziis98 <antonio.delucreziis@gmail.com>
2017-07-17 15:13:07 +0200

    Added logo and n

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const modules = {};
     const repeatLetter = (l) => (n) => _.repeat(l, n);
 
     const genR = (n) => repeatLetter('r')(n);
+    const genN = (n) => repeatLetter('n')(n);
+
+    const genNR = (n) => _.sample([genR, genN])(n);
+
     const genA = (n) => repeatLetter('a')(n * 2 / 3);
     const genH = (n) => repeatLetter('h')(n * 0 + 1);
 
@@ -14,9 +18,9 @@ const modules = {};
     const list = (size) => _.range(size);
     const map = (f) => (list) => list.map(f);
 
-    const randomRepeatedLetter = () => _.sample([genR, genA, genH])(random(8, 10));
+    const randomRepeatedLetter = () => _.sample([genNR, genA, genH])(random(8, 10));
 
-    const gen = () => 'G' + [genR(random(3, 10))].concat(list(random(12, 15)).map(it => randomRepeatedLetter())).join('');
+    const gen = () => 'G' + [_.sample([genR, genN])(random(3, 10))].concat(list(random(12, 15)).map(it => randomRepeatedLetter())).join('');
 
     window.onload = function generate() {
         $container.innerHTML = _.escape(gen());
